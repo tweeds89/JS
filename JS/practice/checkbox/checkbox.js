4337da6f8305972930362f9eb03fcdca5a1a9681
tweeds89 <p.palinski@gmail.com>
2018-02-06 10:26:45 +0100

    total sum

--- JS/practice/checkbox/checkbox.js
+++ JS/practice/checkbox/checkbox.js
@@ -1,17 +1,24 @@
-var checkboxes = document.getElementsByName('additives');
+document.addEventListener('DOMContentLoaded', function(){
+        
+var checkboxes = document.getElementsByName('additives'); 
+var price = document.getElementById("price");
+var toggle = document.getElementById("all");  
+var clear = document.getElementById("clear");    
+var button = document.getElementById('btn');
 
-function toggle(source) {
+//zaznaczenie wszystkich dodatków    
+function check() {
   
-   var clear = document.getElementById("clear"); 
-    
      for(var i=0; i < checkboxes.length; i++) {
    
         checkboxes[i].checked = true;
-        clear.checked = false;
+        clear.checked = false;       
      }
+     get_total();
 }
 
-function uncheck() {
+//odznaczenie wszystkich dodatków    
+function remove() {
    
    var all = document.getElementById("all");
     
@@ -21,32 +28,37 @@ function uncheck() {
    
          checkboxes[i].checked = false;
          all.checked = false;
-      }
-       
+      }      
    }
+    get_total();
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-  
-   var checkboxes = document.getElementsByName('additives'); 
-   var price = document.getElementById("price");
-        
-    for(var i = 0; i < checkboxes.length; i++){
-        checkboxes[i].onchange = function(){
-       
-          get_total();
-        }
-    }
-    function get_total() {
+//podliczenie całkowitej kwoty    
+function get_total() {
 		
-		var sum = 0;
+   var sum = 0;
         
-		for (i=0;i<checkboxes.length;i++) {
+   for (i=0;i<checkboxes.length;i++) {
             
-		  if (checkboxes[i].checked) {
-		  	sum = sum + parseFloat(checkboxes[i].value);
-		  }
-		}
-		price.innerHTML = sum;
-	}
+      if (checkboxes[i].checked) {
+		  	sum = sum + parseFloat(checkboxes[i].value);       
+      }
+   }
+    price.innerHTML = sum;
+   
+    button.onclick = function(){
+        alert('Do zapłaty za dodatki: ' + sum + 'zł');
+    };
+    return sum;
+}
+ 
+    
+    
+    for(var i = 0; i < checkboxes.length; i++){
+        
+        checkboxes[i].addEventListener('change', get_total);     
+    }
+         
+      toggle.addEventListener('click', check);   
+      clear.addEventListener('click', remove); 
 });
